80ddc6549d3ff16b8e31a7df0f169d95a93e402a
ThomasN89-Dev <82367549+ThomasN89-Dev@users.noreply.github.com>
2021-06-23 14:36:15 +0200

    a

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const versus = "versus";
-const Versus = "Versus";
-
 client.on("message", (msg) => {
-  if (msg.content.includes(versus) || msg.content.includes(Versus)) {
-    msg.reply("DON'T PULL A NOMURA ON ME PLS");
-    msg.react("🤡");
-  }
+  const forbiddenWords = ["versus", "Versus", "Nomura", "nomura"];
+  forbiddenWords.map((word) => {
+    console.log(word);
+    if (msg.content.includes(word)) {
+      msg.reply("DON'T PULL A NOMURA ON ME PLS");
+      msg.react("🤡");
+    }
+  });
 });
 
 client.login(process.env.BOT_TOKEN);
