0288efb9fe9254c2f6f0b36f01cf7a4cd9bf0978
ThomasN89-Dev <82367549+ThomasN89-Dev@users.noreply.github.com>
2021-06-23 12:26:55 +0200

    first
    first

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const versus = "versus";
+const Versus = "Versus";
+
+client.on("message", (msg) => {
+  if (msg.content.includes(versus) || msg.content.includes(Versus)) {
+    msg.reply("DON'T PULL A NOMURA ON ME PLS");
+    msg.react("🤡");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
